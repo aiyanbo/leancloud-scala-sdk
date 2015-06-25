b81c28816d3046cb52d8faab14df744fc8751709
Andy Ai <yanbo.ai@gmail.com>
2015-06-25 23:50:08 +0800

    Refactor

--- src/main/scala/org/jmotor/leancloud/LeanCloudClient.scala
+++ src/main/scala/org/jmotor/leancloud/LeanCloudClient.scala
@@ -1,11 +1,37 @@
 package org.jmotor.leancloud
 
+import java.util.concurrent.Future
+
+import com.ning.http.client.{AsyncHttpClient, Response}
+import com.typesafe.config.ConfigFactory
+import org.jboss.netty.handler.codec.http.HttpHeaders
+import org.jmotor.leancloud.utils.MD5Utilities
+
 /**
  * Component:
  * Description:
  * Date: 15/5/12
  * @author Andy Ai
  */
-class LeanCloudClient(val id: String, val key: String) {
+object LeanCloudClient {
+  private val config = ConfigFactory.load()
+  private val id = config.getString("leancloud.app-id")
+  private val key = config.getString("leancloud.app-key")
+  private val asyncHttpClient: AsyncHttpClient = new AsyncHttpClient()
+  private val apiPath = s"${config.getString("leancloud.host")}/${config.getString("leancloud.version")}/classes"
 
+  def insert(className: String, body: String): Future[Response] = execute {
+    val requestBuilder: AsyncHttpClient#BoundRequestBuilder = asyncHttpClient.preparePost(s"$apiPath/$className")
+    requestBuilder.addHeader(HttpHeaders.Names.CONTENT_TYPE, "application/json")
+    requestBuilder.setBody(body)
+  }
+
+  def get(className: String, objectId: String): Future[Response] = execute(asyncHttpClient.prepareGet(s"$apiPath/$className/$objectId"))
+
+  private def execute(requestBuilder: AsyncHttpClient#BoundRequestBuilder): Future[Response] = {
+    val timestamp: Long = System.currentTimeMillis()
+    requestBuilder.addHeader("X-AVOSCloud-Application-Id", id)
+    requestBuilder.addHeader("X-AVOSCloud-Request-Sign", s"${MD5Utilities.encode(s"$timestamp$key")},$timestamp")
+    requestBuilder.execute()
+  }
 }
--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -1,8 +1,7 @@
 import java.util.concurrent.Future
 
-import com.ning.http.client.{AsyncHttpClient, Response}
-import com.typesafe.config.{Config, ConfigFactory}
-import org.jmotor.leancloud.utils.MD5Utilities
+import com.ning.http.client.Response
+import org.jmotor.leancloud.LeanCloudClient._
 import org.scalatest.FunSuite
 
 /**
@@ -13,13 +12,7 @@ import org.scalatest.FunSuite
  */
 class AppTest extends FunSuite {
   test("Make request") {
-    val config: Config = ConfigFactory.load()
-    val asyncHttpClient: AsyncHttpClient = new AsyncHttpClient()
-    val requestBuilder: AsyncHttpClient#BoundRequestBuilder = asyncHttpClient.prepareGet("https://leancloud.cn:443/1.1/classes/stacks/scala")
-    val timestamp: Long = System.currentTimeMillis()
-    requestBuilder.addHeader("X-AVOSCloud-Application-Id", config.getString("leancloud.app-id"))
-    requestBuilder.addHeader("X-AVOSCloud-Request-Sign", s"${MD5Utilities.encode(s"$timestamp${config.getString("leancloud.app-key")}")},$timestamp")
-    val f: Future[Response] = requestBuilder.execute()
+    val f: Future[Response] = get("stacks", "scala")
     val response: Response = f.get()
     println(response.getResponseBody)
   }
