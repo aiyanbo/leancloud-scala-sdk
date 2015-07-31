b224016ddbfa1c7df14e77de332bd76666c19e9b
Andy Ai <yanbo.ai@gmail.com>
2015-07-31 12:31:48 +0800

    Test case assert

--- src/test/scala/StacksSpec.scala
+++ src/test/scala/StacksSpec.scala
@@ -1,6 +1,5 @@
-import org.scalatest.FunSuite
-
 import org.jmotor.conversions.JsonConversions._
+import org.scalatest.FunSuite
 
 /**
  * Component:
@@ -14,13 +13,13 @@ class StacksSpec extends FunSuite {
     val resp = """{"results": [{"createdAt":"2015-07-29T09:33:58.189Z","updatedAt":"2015-07-29T09:33:58.189Z","objectId":"55b89e0600b0ed9c142e221a"}, {"createdAt":"2015-07-29T09:33:58.189Z","updatedAt":"2015-07-29T09:33:58.189Z","objectId":"55b89e0600b0ed9c142e221c"}]}"""
     val r = """"objectId": *"(\w+)"""".r
     val ids = for (id <- r.findAllMatchIn(resp)) yield id group 1
-    println(s"ids is empty: ${ids.isEmpty}")
-    ids.foreach(id => println(id))
+    assertResult(false)(ids.isEmpty)
+    assertResult("55b89e0600b0ed9c142e221a, 55b89e0600b0ed9c142e221c")(ids.reduce(_ + ", " + _))
   }
 
   test("To json string") {
-    val map = Map("username" -> "aiyanbo", "objectId" -> "55667", "age" -> 15, "hobbies" -> List("java", "scala", "clojure"), "others" -> Map("gate" -> 3))
-    println(mapToJsonString(map))
+    val map = Map("username" -> "aiyanbo", "objectId" -> "55b89e0600b0ed9c142e221a", "age" -> 15, "hobbies" -> List("java", "scala", "clojure"), "others" -> Map("gate" -> 3))
+    assertResult( """{"hobbies":["java","scala","clojure"],"username":"aiyanbo","others":{"gate":3},"age":15,"objectId":"55b89e0600b0ed9c142e221a"}""")(mapToJsonString(map))
   }
 
 }
