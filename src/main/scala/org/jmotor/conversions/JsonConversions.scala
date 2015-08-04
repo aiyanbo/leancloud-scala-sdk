d6ce9796802f58880e313bbce26e9f91e3ee42df
Andy Ai <yanbo.ai@gmail.com>
2015-08-04 16:17:00 +0800

    DateTimeFormatter

--- src/main/scala/org/jmotor/conversions/JsonConversions.scala
+++ src/main/scala/org/jmotor/conversions/JsonConversions.scala
@@ -1,6 +1,8 @@
 package org.jmotor.conversions
 
 import java.text.SimpleDateFormat
+import java.time.{ LocalDate, LocalTime, ZoneId, LocalDateTime }
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 import scala.language.implicitConversions
@@ -13,20 +15,21 @@ import scala.language.implicitConversions
  * @author Andy.Ai
  */
 object JsonConversions {
-  private val separator = ", "
+  private val separator = ","
 
   implicit def mapToJsonString(filters: Map[String, Any]): String =
     s"{${
       val toJsonNodeString: PartialFunction[Any, String] = {
-        case n: Number  ⇒ n.toString
-        case b: Boolean ⇒ b.toString
-        case e: String  ⇒ s""""$e""""
-        case d: Date    ⇒ s""""${new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(d)}""""
+        case n: Number          ⇒ n.toString
+        case b: Boolean         ⇒ b.toString
+        case e: String          ⇒ s""""$e""""
+        case d: Date            ⇒ s""""${DateTimeFormatter.ISO_INSTANT.format(d.toInstant)}""""
+        case ldt: LocalDateTime ⇒ s""""${ldt.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_INSTANT)}""""
       }
       filters.foldLeft("")(
         (l, kv) ⇒ l + (if (l.isEmpty) "" else separator) + s""""${
           kv._1
-        }": ${
+        }":${
           toJsonNodeString.applyOrElse(kv._2, PartialFunction[Any, String] {
             case m: Map[String, _] ⇒ mapToJsonString(m)
             case i: Iterable[_]    ⇒ s"[${i.map(toJsonNodeString).reduce(_ + separator + _)}]"
